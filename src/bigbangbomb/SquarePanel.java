eef281b1b39dad315b9e954388ec8a75cc086143
i-am-prinx <geek_200@outlook.com>
2018-09-06 14:31:30 +0100

    added SquarePanel class

--- src/bigbangbomb/SquarePanel.java
+++ src/bigbangbomb/SquarePanel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bigbangbomb;
 
 import java.awt.Color;
@@ -11,13 +6,6 @@ import javax.swing.JPanel;
 /**
  *
  * @author Agbonxoft Prince
- */
-
-
-
-
-/**
- * this will create a square panel which will be mounted on our application frame
  */
 
 
