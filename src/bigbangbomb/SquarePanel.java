ed81e1e7674581a8287aa07995b36e3f41293d66
i-am-prinx <geek_200@outlook.com>
2018-09-07 16:21:48 +0100

    added eventlistener which listens to mouse events

--- src/bigbangbomb/SquarePanel.java
+++ src/bigbangbomb/SquarePanel.java
@@ -3,6 +3,8 @@ package bigbangbomb;
 import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  *
@@ -10,10 +12,38 @@ import javax.swing.JPanel;
  */
 
 
-public class SquarePanel extends JPanel {
+public class SquarePanel extends JPanel implements MouseListener {
     SquarePanel() {
+        super();
+        this.addMouseListener(this);
         setBorder(BorderFactory.createLineBorder(Color.white));
         setBackground(Color.black);
         System.out.println("5\tcreateNextpanel Called and Panel is returned");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        JPanel src = (JPanel)e.getSource();
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Mouse Pressed");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("Mouse Released");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("Mouse Entered");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("Mouse Exited");
     }
 }
