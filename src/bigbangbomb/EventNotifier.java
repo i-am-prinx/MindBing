6ba557b41a6801166f84ec11f0a058bbecd25636
i-am-prinx <geek_200@outlook.com>
2018-09-13 00:32:18 +0100

    restarted game logic :( display of life successful (:()==<>)

--- src/bigbangbomb/EventNotifier.java
+++ src/bigbangbomb/EventNotifier.java
@@ -19,14 +19,18 @@ import net.miginfocom.swing.MigLayout;
  * @author Agbonxoft Prince
  */
 public class EventNotifier {
-    private static JPanel squareContainer = SquareFrame.squareContainer();
-    private static int WIDTH = squareContainer.getWidth();
-    private static int lifeRemaining = Player.getGameLife();
-    private static JLabel lifeLabel, lifeNotify, bombAroundLabel, bombAroundNotify;
+    
     private static JPanel SMFN = new JPanel();
+    private static JPanel SMFRL = new JPanel();
+    private static int gameLife = Player.getGameLife();
+    private static JPanel lifeNotifyPanel = new JPanel(); 
     private static JPanel bombAroundSquare = new JPanel();
+    private static int bombAround = SquarePanel.getBombAround();
+    private static JLabel lifeLabel, lifeNotify, bombAroundLabel, bombAroundNotify;
     
     
+    //******************** Show Message For Neutralize( SMFN )
+    //*************** Show Message For Reduced Life ( SMFRL )
     
     /**
      * will be displayed before the grid will be displayed on the game menu
@@ -36,34 +40,22 @@ public class EventNotifier {
     public static JPanel notificationBar(){
         JPanel notifierBar = new JPanel();
         notifierBar.setLayout(new MigLayout());
-        notifierBar.setSize(WIDTH, 10);
         notifierBar.setBackground(Color.darkGray);
         
-        
-        
-        lifeNotify = new JLabel("GuessLife : ");
-        lifeNotify.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));
-        lifeNotify.setForeground(Color.magenta);
-        lifeNotify.setBounds(10, 10, 10, 10);
-        
-        lifeLabel = new JLabel(lifeRemaining + "");
-        lifeLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));
-        lifeLabel.setForeground(Color.GREEN);
-        lifeLabel.setBounds(15, 15, 65, 15);
-        if ( lifeRemaining < 3){
-            lifeLabel.setForeground(Color.RED);
-            lifeLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 18));
-        }
-        
         ConfiguredShowMsgForNeutralize();
+        ConfiguredShowMsgForReducedLife();
         
-        notifierBar.add(lifeNotify);
-        notifierBar.add(lifeLabel, "push, push" );
-        notifierBar.add(bombAroundSquare, "wrap, push");
-        notifierBar.add(SMFN, "span");
+        setLife(gameLife);
+        
+        setBombAroundSquare(bombAround);
+                
+        
+        notifierBar.add(bombAroundSquare, "push");
+        notifierBar.add(lifeNotifyPanel, "wrap" );
+        notifierBar.add(SMFN);
+                notifierBar.add(SMFRL);
         return notifierBar;
-    }
-    
+    } 
     
     /**
      * this will output a message anytime a bomb was successfully neutralized.
@@ -74,11 +66,23 @@ public class EventNotifier {
     private static void ConfiguredShowMsgForNeutralize( ){
         SMFN.setBackground(Color.darkGray);
         
-        JLabel neutralizeMsg = new JLabel("Bomb was succefully Neutralized");
-        neutralizeMsg.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));
+        JLabel neutralizeMsg = new JLabel("Bomb Neutralized");
+        neutralizeMsg.setFont(new Font(Font.MONOSPACED, Font.BOLD, 15));
         neutralizeMsg.setForeground(Color.YELLOW);
         SMFN.setVisible(false);
         SMFN.add(neutralizeMsg);
+    }
+    
+    
+    
+    private static void ConfiguredShowMsgForReducedLife( ){
+        SMFRL.setBackground(Color.darkGray);
+        
+        JLabel reducedLifeMsg = new JLabel("life reduced");
+        reducedLifeMsg.setFont(new Font(Font.MONOSPACED, Font.BOLD, 15));
+        reducedLifeMsg.setForeground(Color.YELLOW);
+        SMFRL.setVisible(false);
+        SMFRL.add(reducedLifeMsg);
     }
     
     
@@ -93,23 +97,49 @@ public class EventNotifier {
      * @param txt 
      *    this is the text that will suffice the amount of bomb.
      */
-    public static void setBombAroundSquare( int bombsAround, String txt ){
-        bombAroundSquare.setBackground(Color.darkGray);
+    public static void setBombAroundSquare( int bombsAround ){
         bombAroundSquare.setLayout(new MigLayout());
+        bombAroundSquare.setBackground(Color.darkGray);
         
-        bombAroundNotify = new JLabel();
-        bombAroundNotify.setText(txt);
-        bombAroundNotify.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));
+        bombAroundNotify = new JLabel("Bomb around : ");
+        bombAroundNotify.setFont(new Font(Font.MONOSPACED, Font.BOLD, 15));
         bombAroundNotify.setForeground(Color.magenta);
         bombAroundNotify.setBounds(10, 10, 10, 10);
         
         bombAroundLabel = new JLabel(" "+ bombsAround);
-        bombAroundLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 18));
+        bombAroundLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 15));
         bombAroundLabel.setForeground(Color.GREEN);
         bombAroundLabel.setBounds(15, 15, 65, 15);
         
         bombAroundSquare.add(bombAroundNotify);
         bombAroundSquare.add(bombAroundLabel);
+    }
+    
+    
+     public static void setLife( int lifeRemaining ){
+        lifeNotifyPanel.setLayout(new MigLayout());
+        lifeNotifyPanel.setBackground(Color.darkGray);
+        
+        lifeNotify = new JLabel("life : ");
+        lifeNotify.setFont(new Font(Font.MONOSPACED, Font.BOLD, 15));
+        lifeNotify.setForeground(Color.magenta);
+        lifeNotify.setBounds(10, 10, 10, 10);
+        
+        lifeLabel = new JLabel(lifeRemaining + " ");
+        lifeLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 15));
+        lifeLabel.setForeground(Color.GREEN);
+        lifeLabel.setBounds(15, 15, 65, 15);
+        if ( lifeRemaining < 3){
+            lifeLabel.setForeground(Color.RED);
+            lifeLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 15));
+        }
+        
+        lifeNotifyPanel.add(lifeNotify);
+        lifeNotifyPanel.add(lifeLabel);
+    }
+    
+    public static void resetLifeRemaining( ){
+        lifeNotifyPanel.removeAll();
     }
     
     /**
@@ -136,4 +166,20 @@ public class EventNotifier {
     public static void showSMFN(){
         SMFN.setVisible(true);
     }
+    
+    
+     /**
+     * this will be used to hide messageForNeutralize from displaying when not needed
+     */
+    public static void hideSMFRL(){
+        SMFRL.setVisible(false);
+    }
+    
+    /**
+     * this will be used to show messageForNeutralize ( so it displays ) when needed.
+     */
+    public static void showSMFRL(){
+        SMFRL.setVisible(true);
+    }
+    
 }
--- src/bigbangbomb/SquareFrame.java
+++ src/bigbangbomb/SquareFrame.java
@@ -1,53 +1,42 @@
-
 package bigbangbomb;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
-
-
 
 /**
  *
  * @author Agbonxoft Prince
  */
-
-
 /**
  * frame that displays the grid system.
  */
-public class SquareFrame extends JFrame{
+public class SquareFrame extends JFrame {
+
     private static JPanel panel = new JPanel();
     private static JPanel notifyBar = EventNotifier.notificationBar();
-    private static JPanel notifyHolder = new JPanel();
-    
-    
-    SquareFrame( ) {
-        super("*- Mind~Bing -*");       
+
+    SquareFrame() {
+        super("*- Mind~Bing -*");
         setLayout(new BorderLayout());
-        
+
         JPanel[][] fp = new GridDimension().callNextGrid();
-        
+
         panel.setLayout(new MigLayout());
-        
-        for ( int i = 0 ; i < fp.length; i++) {
-            for ( int j = fp[i].length-1; j > 0 ; j-- ) {
-               if ( j == 1 ){
+
+        for (int i = 0; i < fp.length; i++) {
+            for (int j = fp[i].length - 1; j > 0; j--) {
+                if (j == 1) {
                     System.out.print("#  \n");
                     panel.add(fp[i][j], "width 30:80:80, height 30:80:80, wrap");
-               }
-               else {
+                } else {
                     System.out.print("#  ");
                     panel.add(fp[i][j], "width 30:80:80, height 30:80:80");
-                } 
+                }
             }
         }
-        
-        int MAX_HEIGHT = panel.getHeight();
-        int MAX_WIDTH = panel.getWidth();
-        
+
         add(panel, BorderLayout.CENTER);
         add(notifyBar, BorderLayout.SOUTH);
         setVisible(true);
@@ -55,26 +44,25 @@ public class SquareFrame extends JFrame{
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
-    
-    
+
     /**
      * this is returned so it's attributes can be used in other class files
      * attributes like ( width and height )
+     *
      * @return JPanel
      */
-    public static JPanel squareContainer( ){
+    public static JPanel squareContainer() {
         return panel;
     }
-    
-        
-    
-    /*** this main method will be removed from this file, it's just for testing
+
+    /**
+     * * this main method will be removed from this file, it's just for testing
      * if the grid system is working as expected...
-     * @param args 
+     *
+     * @param args
      */
-    public static void main (String args[]){
+    public static void main(String args[]) {
         SquareFrame mindBing = new SquareFrame();
-        mindBing.setSize(440, 500);     // increases the width to fit notifier text
+        mindBing.setSize(470, 500);     // increases the width to fit notifier text
     }
 }
